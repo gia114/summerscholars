fa9d180157d0183aa6bea3ba893b331cc511638a
gia114 <140085586+gia114@users.noreply.github.com>
2023-07-20 10:00:56 -0400

    Add files via upload

---
+++ gia torres/intro-gia/intro-gia/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  intro-gia
+//
+//  Created by SMART Scholars on 7/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var textField: UITextField!
+
+    override func viewDidLoad() {
+                super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submit(_ sender: UIButton) {
+        if let newTitle = textField.text {
+            appTitle.text = newTitle
+        }
+    }
+    
+}
+
